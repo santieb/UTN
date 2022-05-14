044faa95af4ec43ac6d67d8c37da665ab2fef113
unknown <santiagonicolasbarreto@gmail.com>
2022-05-13 23:17:27 -0300

    exercise php

---
+++ Ejercicio veterinaria/conexion.php
@@ -0,0 +1,11 @@
+<?php
+  $con=mysqli_connect("localhost", "root", "", "veterinaria");
+
+?>
+
+//formulario registro
+responsables
+hc
+marcota
+
+listado de todos
---
+++ ejercicios-jquery/ej5/js/index.js
@@ -0,0 +1,5 @@
+const prueba = () => {
+  $("#res").val("rdsad")
+}
+
+prueba()
